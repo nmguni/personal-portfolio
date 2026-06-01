2490d48031f3c0a71470f872593792c2920e59c9
Ndumiso Mguni <Ndumiso29@gmail.com>
2026-06-01 13:14:55 -0400

    navbar complete - both desktop and mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { Hero } from "@/sections/Hero"
+import { About } from "@/sections/About"
+import { Projects } from "@/sections/Projects"
+import { Contact } from "@/sections/Contact"
+import { Experience } from "@/sections/Experience"
+import { Testimonials } from "@/sections/Testimonials"
+
+import { Navbar } from "@/layout/Navbar"
+
 export const App = () => {
   return (
-    <div>Portfolio</div>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main>
+    </div>
   )
 }
